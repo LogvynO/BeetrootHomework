acb2ae42d2e5781fed76a4e15487668037a7d087
“LogvynO” <logvyn.o@gmail.com>
2022-06-05 20:24:24 +0200

    lesson14 added

---
+++ lessons/lesson14/indexs.js
@@ -0,0 +1,34 @@
+//задача 1
+/* let a = 0.1;
+let b = 0.2;
+let result = a + b;
+console.log(result.toFixed(1));
+ */
+
+//задача 2
+/* let a = '1';
+let b = 2;
+console.log(a+b) */
+
+//задача 3
+/* let a = prompt('подайте розмір фрешки в GB');
+let b = 820;
+let c = a * 1000;
+let d = c / b;
+console.log(d.toFixed(2));
+alert('кількість файлів які можна змістити на флешці ' + d.toFixed(2)); */
+
+//задача 4
+/* let a = prompt('сумма в гаманці:');
+let b = prompt('ціна шоколадки:');
+let c = a / b;
+let d = a % b;
+alert('стільки шоколадок можна купити ' + Math.floor(c));
+alert('здача ' + d);
+console.log(Math.floor(c));
+console.log(d); */
+
+//задача 5
+/* let a = prompt('подайте тризначне число:');
+alert('дзеркальне число: ' + a.split("").reverse().join(""));
+console.log(a.split("").reverse().join("")); */
