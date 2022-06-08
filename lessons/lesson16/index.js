94a26a75bbd86383cd661e8fdf7e33660bb0d66c
“LogvynO” <logvyn.o@gmail.com>
2022-06-08 23:12:43 +0200

    functions uploaded

---
+++ lessons/lesson16/index.js
@@ -0,0 +1,69 @@
+// Task 1
+/* 
+1) function functionName (parameter items) {function body};
+2) let functionName = function (parameter items) {function body};
+3) let functionName = (arguments,..) => {function body};
+*/
+
+// Task 3
+/* let a = prompt('provide first number');
+let b = prompt('provide second number');
+
+function numVerify 
+(a, b) 
+{
+    if (a < b) {console.log('-1')}
+    else if (a > b) {console.log('1')}
+    else {console.log('0')}
+};
+
+numVerify(a, b); */
+
+
+
+// Task 5
+/* let a = prompt('provide first number');
+let b = prompt('provide second number');
+let c = prompt('provide third number');
+
+function add(string) {
+    console.log(a + b + c)
+};
+add(a, b, c); */
+
+
+
+// Task 6
+/* let a = prompt('provide length of one side');
+let b = prompt('provide length of other side');
+
+function perimeter (a, b)
+{
+    if (a != 0 && b != 0) {console.log(a*b);}
+    else {
+        console.log(a*a)
+        }
+};
+
+perimeter(a, b); */
+
+
+
+// Task 7
+/* function perfection (num)
+{
+    let  x = 0;
+    for
+    (let i=1; i <= num / 2; i++)
+    {
+        if(num % i == 0)
+        {x = x + i;}
+    }
+    if (x == num && x !== 0)
+    {console.log("perfection");}
+    else {console.log("some bullshit, try again");}
+};
+
+let num = prompt()
+
+perfection(num); */
